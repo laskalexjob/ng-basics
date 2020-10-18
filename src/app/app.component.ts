20a806792691bc7ffc58889c979e3d0e901dc7b5
laskalexjob <laskalexjob@gmail.com>
2020-10-18 18:34:47 +0300

    Added iteration directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Card{
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +14,13 @@ export class AppComponent {
 
   toggle = true;
 
+  cards: Card[] = [
+    {title: 'Card 1', text: 'First card text'},
+    {title: 'Card number two', text: 'Second card text'},
+    {title: 'Last card', text: 'Third card text'}
+  ]
+
   toggleCards(){
     this.toggle = !this.toggle;
   }
-
 }
