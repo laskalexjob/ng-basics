079f1512150023536d0ad182a02cbff1bb210285
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-20 16:39:59 +0300

    ngSwitch directive usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Dynamic title';
-  imgDefault =
-    'https://cdn2.iconfinder.com/data/icons/designer-skills/128/react-512.png';
-  imgReact = this.imgDefault;
-  imgAngular =
-    'https://cdn.freebiesupply.com/logos/large/2x/angular-icon-1-logo-png-transparent.png';
-  inputVal = '';
-  toggleSwitcher: boolean = true;
+  toggleSwitcher: any = true;
 
   toggleSwith() {
     this.toggleSwitcher = !this.toggleSwitcher;
-  }
-  constructor() {
-    setTimeout(() => {
-      this.imgReact = this.imgAngular;
-    }, 3000);
-  }
-
-  onBlur(str) {
-    this.inputVal = str;
-  }
-
-  onInput(event) {
-    console.log('Event', event);
-    this.inputVal = event.target.value;
   }
 }
