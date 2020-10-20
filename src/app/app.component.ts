2f1fb38684d53be20c38d7e7e68372e4483115b7
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-20 17:23:40 +0300

    Pipe example for date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  arr: number[] = [1, 2, 3, 4, 5, 6, 7];
-
-  objs = [
-    {
-      title: 'Post1',
-      author: 'Alex',
-      comments: [
-        { name: 'post1', text: 'some' },
-        { name: 'post1', text: 'some' },
-        { name: 'post1', text: 'some' },
-      ],
-    },
-    {
-      title: 'Post2',
-      author: 'Alex 2',
-      comments: [
-        { name: 'post2', text: 'some 2' },
-        { name: 'post2', text: 'some 2' },
-        { name: 'post2', text: 'some 2' },
-      ],
-    },
-  ];
+  date: Date = new Date();
 }
