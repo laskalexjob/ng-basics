2e60fe238e3d1f9629085323a9031bbc3fc3ed9f
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-20 18:51:43 +0300

    Parameters providing to component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  posts: Post[] = [
+    {
+      title: 'I want to learn angular components',
+      text: "I'm still learning",
+      id: 1,
+    },
+    {
+      title: 'I also want to learn Core',
+      text: 'I am stiil do this too',
+      id: 2,
+    },
+  ];
+}
