c138ed212732179819819b3182c1c2640ecef6e5
laskalexjob <laskalexchill@gmail.com>
2020-10-21 16:07:40 +0300

    ContentChild access to native element.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChild, ElementRef, Input, OnInit } from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -8,6 +8,9 @@ import { Post } from '../app.component';
 })
 export class PostComponent implements OnInit {
   @Input() post: Post;
+  @ContentChild('info', { static: true }) infoRef: ElementRef
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.infoRef.nativeElement);
+  }
 }
