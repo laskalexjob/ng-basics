3434b831bfa9629e18773f4c4741d238a77adc44
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-24 14:07:12 +0300

    Controls and input bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null)
+    });
+  }
+
+  submit() {
+    console.log('Form submitted', this.form);
+    const values = { ...this.form.value }
+    console.log(values);
+  }
 }
 
