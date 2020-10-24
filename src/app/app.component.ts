4be96e98b6624b296b346c630cee52de0fd7d1ed
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-24 17:13:13 +0300

    Async validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
         Validators.email,
         Validators.required,
         MyValidators.restrictedEmail
-      ]),
+      ], MyValidators.uniqueEmail),
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(6)
--- src/app/my.validators.ts
+++ src/app/my.validators.ts
@@ -1,4 +1,5 @@
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 export class MyValidators {
     static restrictedEmail(control: FormControl): { [key: string]: boolean } {
@@ -6,5 +7,17 @@ export class MyValidators {
             return { restrictedEmail: true }
         }
         return null
+    }
+
+    static uniqueEmail(control: FormControl): Promise<any> | Observable<any> {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                if (control.value == 'async@mail.ru') {
+                    resolve({ uniqueEmail: true })
+                } else {
+                    resolve(null)
+                }
+            }, 1000);
+        })
     }
 }
