89209ae56bdf6691cd45e8bf3648e54b41ef2e5e
laskalexjob <laskalexchill@gmail.com>
2020-10-24 12:47:07 +0300

    Injection service into another service

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,16 +1,20 @@
+import { LogService } from './log.service';
 import { Injectable } from '@angular/core'
 
 @Injectable({
     providedIn: 'root'
 })
 export class AppCounterService {
+    constructor(private logService: LogService) { }
     counter = 0
 
     increment() {
         this.counter++
+        this.logService.log(this.counter)
     }
 
     decrement() {
         this.counter--
+        this.logService.log(this.counter)
     }
 }
