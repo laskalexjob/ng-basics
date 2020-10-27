01a7e4414131e33ded235b303a3f30272a09a1fe
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-27 13:52:58 +0300

    Basic crud operations with http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core'
+import { delay } from "rxjs/operators";
 
 export interface Todo {
   completed: boolean,
@@ -14,15 +15,49 @@ export interface Todo {
 })
 export class AppComponent implements OnInit {
 
+  todoTitle = ''
+  loading = false
   todos: Todo[] = []
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    this.fetchTodos()
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        console.log('todo', todo);
+        this.todos.push(todo)
+        this.todoTitle = ''
+      })
+  }
+
+  fetchTodos() {
+    this.loading = true
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .pipe(delay(1500))
       .subscribe(todos => {
         console.log('Response', todos)
         this.todos = todos
+        this.loading = false
+      })
+  }
+
+  removeTodo(id: number) {
+    this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .subscribe(() => {
+        this.todos = this.todos.filter(i => i.id !== id)
       })
   }
 }
