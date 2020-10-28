9642a4fe359a4cd36eb6e7320e29fc8aea8e129f
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-29 00:14:42 +0300

    Child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AboutExtraComponent } from './about-extra/about-extra.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,13 +7,18 @@ import { AboutComponent } from './about/about.component';
 import { PostComponent } from './post/post.component';
 
 // http://localhost:4200/ -> HomeComponent
-// http://localhost:4200/ -> AboutComponent
-// http://localhost:4200/ -> PostsComponent
+// http://localhost:4200/posts -> PostsComponent
+// http://localhost:4200/about -> AboutComponent
+// http://localhost:4200/posts/extra -> AboutComponent
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
+    {
+        path: 'about', component: AboutComponent, children: [
+            { path: 'extra', component: AboutExtraComponent }
+        ]
+    },
     { path: 'posts', component: PostsComponent },
-    { path: 'posts/:id', component: PostComponent },
+    { path: 'posts/:id', component: PostComponent }
 ]
 
 @NgModule({
