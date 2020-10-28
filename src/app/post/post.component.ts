736a6a11834055b92f64a250bf4411cc77a02566
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-10-28 18:53:40 +0300

    Router params navigation

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core'
+import { Post, PostsService } from './../posts.service';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Params, Routes, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
+  post: Post
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log('Params', params);
+      this.post = this.postsService.getById(+params.id)
+    })
+  }
+
+  loadPosts() {
+    this.router.navigate(['/posts', 44])
+  }
 }
